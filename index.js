3561e6ee9bb7c31137f8eea15a047f3683804cf5
Colleen <colleenhiggins87@gmail.com>
2019-06-22 21:27:16 -0700

    added screenshot

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 /*disable eslint*/
 /*use global $*/
 function main (){
-  
+  document.getElementById('quiz-app-github').onclick = function () {
+    location.href = 'https://github.com/thinkful-ei-emu/ex-module2-blake-colleen';
+  };
+
+  document.getElementById('quiz-app-page').onclick = function () {
+    location.href = '#';
+  };
 }
 
 
