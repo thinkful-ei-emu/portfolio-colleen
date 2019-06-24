2600a36e8ddfec4cf8de97901f3d99409ff68fd9
Colleen <colleenhiggins87@gmail.com>
2019-06-24 16:31:03 -0700

    modified header href for visibility

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function main (){
   document.getElementById('quiz-app-page').onclick = function () {
     location.href = 'https://coggins87.github.io/quizapp/';
   };
+
+  $("header .nav a[href!=#]").each(function(){
+    $($(this).attr("href")).css("padding-top", "40px").prev().css("margin-bottom", "-40px");
+});
 }
 
 
