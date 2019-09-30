0a2151b7b9183849cdf427604db5d7807b892f47
Colleen <colleenhiggins87@gmail.com>
2019-09-29 17:41:57 -0700

    add project, new gh logo

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ function main (){
 setTimeout(function(){$("#info").fadeIn()}, 3000)
 
   document.getElementById('quiz-app-github').onclick = function () {
-    location.href = 'https://github.com/thinkful-ei-emu/ex-module2-blake-colleen';
+   window.open('https://github.com/thinkful-ei-emu/ex-module2-blake-colleen');
   };
 
   document.getElementById('quiz-app-page').onclick = function () {
-    location.href = 'https://coggins87.github.io/quizapp/';
+   window.open('https://coggins87.github.io/quizapp/');
   };
 
   document.getElementById('amrapp-app-client-github').onclick = function () {
-    location.href = 'https://github.com/thinkful-ei-emu/colleen-capstone-client';
+   window.open('https://github.com/thinkful-ei-emu/colleen-capstone-client');
   };
   document.getElementById('amrapp-app-api-github').onclick = function () {
-    location.href = 'https://github.com/thinkful-ei-emu/amrap-api-colleen';
+   window.open('https://github.com/thinkful-ei-emu/amrap-api-colleen');
   };
   document.getElementById('amrapp-app-page').onclick = function () {
-    location.href = 'https://amrap-client-colleen.colleenhiggins87.now.sh/';
+   window.open('https://amrap-client-colleen.colleenhiggins87.now.sh/');
   };
 
   document.getElementById('sr-app-client-github').onclick = function () {
-    location.href = 'https://github.com/thinkful-ei-emu/spaced-repetition-MAC-client';
+   window.open('https://github.com/thinkful-ei-emu/spaced-repetition-MAC-client');
   };
   document.getElementById('sr-app-api-github').onclick = function () {
-    location.href = 'https://github.com/thinkful-ei-emu/spaced-repetition-MAC-api';
+   window.open('https://github.com/thinkful-ei-emu/spaced-repetition-MAC-api');
   };
   document.getElementById('sr-app-page').onclick = function () {
-    location.href = 'https://spaced-repetition.mac-thinkful.now.sh/';
+   window.open('https://spaced-repetition.mac-thinkful.now.sh/');
   };
 
  
